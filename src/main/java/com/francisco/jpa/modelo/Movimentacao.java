bdf35639718aab680d78b3473a6d3209a87e1bd1
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-21 07:01:53 -0300

    Modelando a movimentação

---
+++ src/main/java/com/francisco/jpa/modelo/Movimentacao.java
@@ -0,0 +1,64 @@
+package com.francisco.jpa.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Movimentacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TipoMovimentacao tipoMovimentacao;
+
+    private BigDecimal valor;
+
+    private LocalDateTime data;
+
+    private String descricao;
+
+    // private Conta conta;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public TipoMovimentacao getTipoMovimentacao() {
+        return tipoMovimentacao;
+    }
+
+    public void setTipoMovimentacao(TipoMovimentacao tipoMovimentacao) {
+        this.tipoMovimentacao = tipoMovimentacao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/com/francisco/jpa/modelo/TipoMovimentacao.java
@@ -0,0 +1,5 @@
+package com.francisco.jpa.modelo;
+
+public enum TipoMovimentacao {
+    ENTADA, SAIDA
+}
