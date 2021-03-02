9eb81deba0760b6e57e6e95c586f5cec7e4280de
Francisco Sousa <franciscoeds1987@gmail.com>
2021-03-02 09:23:47 -0300

    Query com ORDER BY DESC

--- src/main/java/com/francisco/jpa/teste/TesteJPQL.java
+++ src/main/java/com/francisco/jpa/teste/TesteJPQL.java
@@ -3,13 +3,9 @@ package com.francisco.jpa.teste;
 import com.francisco.jpa.modelo.Conta;
 import com.francisco.jpa.modelo.Movimentacao;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.*;
 import java.util.List;
 
-@SuppressWarnings("unchecked")
 public class TesteJPQL {
 
     public static void main(String[] args) {
@@ -20,14 +16,15 @@ public class TesteJPQL {
         Conta conta = new Conta();
         conta.setId(1L);
 
-        String jpql = "select m from Movimentacao m where m.conta = :pconta";
+        String jpql = "select m from Movimentacao m where m.conta = :pconta order by m.valor desc";
 
-        Query query = em.createQuery(jpql);
+        TypedQuery<Movimentacao> query = em.createQuery(jpql, Movimentacao.class);
         query.setParameter("pconta", conta);
         List<Movimentacao> resultList = query.getResultList();
 
         for (Movimentacao movimentacao : resultList) {
             System.out.println("Descrição: " + movimentacao.getDescricao());
+            System.out.println("Valor: " + movimentacao.getValor());
             System.out.println("Tipo: " + movimentacao.getTipoMovimentacao());
         }
     }
