c2d293aa45c3d8a0af517373efe329f89a1baacf
Francisco Sousa <franciscoeds1987@gmail.com>
2021-03-10 11:23:37 -0300

    Carregamento preguiçoso

---
+++ src/main/java/com/francisco/jpa/teste/TestaRelatorioDasMovimentacoes.java
@@ -0,0 +1,31 @@
+package com.francisco.jpa.teste;
+
+import com.francisco.jpa.modelo.Conta;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class TestaRelatorioDasMovimentacoes {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+        EntityManager em = emf.createEntityManager();
+
+        String jpql = "select c from Conta c";
+        TypedQuery<Conta> query = em.createQuery(jpql, Conta.class);
+
+        List<Conta> contas = query.getResultList();
+
+        for (Conta conta :
+             contas) {
+            System.out.println("Titular: " + conta.getTitular());
+            System.out.println("Agência: " + conta.getAgencia());
+            System.out.println("Número: " + conta.getNumero());
+            System.out.println("Movimentações: " + conta.getMovimentacoes());
+        }
+    }
+}
